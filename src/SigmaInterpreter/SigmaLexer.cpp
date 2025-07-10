2d19ab9537d5d8fa621f9fe7791eb079836b344f
SkyPractice <skypractice982@gmail.com>
2025-07-10 15:09:59 +0300

    sigmalexer made

---
+++ src/SigmaInterpreter/SigmaLexer.cpp
@@ -0,0 +1,71 @@
+#include "SigmaLexer.h"
+#include <cctype>
+#include <stdexcept>
+#include <string>
+
+std::vector<SigmaToken> SigmaLexer::tokenize(std::string code) {
+    current_char = code[0];
+    current_pos = 0;
+    actual_code = &code;
+
+    std::vector<SigmaToken> tokens;
+
+    while(current_char != '\0' && current_pos < code.size() - 1){
+        while(skip_chars.contains(current_char))
+            advance();
+
+        if(isalpha(current_char)){
+            std::string str;
+            while(isalpha(current_char)){
+                str.push_back(current_char);
+                advance();
+            }
+
+            if(known_tokens.contains(str))
+                tokens.push_back({ known_tokens[str], str });
+            else tokens.push_back({ Identifier, str });
+        }
+        else if (isdigit(current_char)){
+            std::string str;
+
+            while(isdigit(current_char) || current_char == '.' ){
+                str.push_back(current_char);
+                advance();
+            }
+            tokens.push_back({ Number, str });
+        }
+        else {
+            std::string the_str;
+            the_str.push_back(current_char);
+
+            if((the_str == ">" || the_str == "<" || the_str == "!") && code[current_pos + 1] == '='){
+                the_str.push_back('=');
+                advance();
+            } else if (the_str == ">" && code[current_pos + 1] == '>'){
+                the_str.push_back('>');
+                advance();
+            } else if (the_str == "<" && code[current_pos + 1] == '<'){
+                the_str.push_back('<');
+                advance();
+            } else if (the_str == "&" && code [current_pos + 1] == '&'){
+                the_str.push_back('&');
+                advance();
+            } else if (the_str == "|" && code [current_pos + 1] == '|'){
+                the_str.push_back('&');
+                advance();
+            }
+
+            if(known_tokens.contains(the_str))
+                tokens.push_back({ known_tokens[the_str], the_str });
+            else throw std::runtime_error("Unknown Sigma Token In Lexer Line " + 
+                std::to_string(__LINE__) + ", Token " + the_str);
+            
+            advance();
+        }
+    }
+
+
+    actual_code = nullptr;
+    current_char = '0';
+    current_pos = 0;
+};
--- src/SigmaInterpreter/SigmaLexer.h
+++ src/SigmaInterpreter/SigmaLexer.h
@@ -1,6 +1,52 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+enum SigmaTokenType{
+    Var, Number, Str, Bool, Char, Identifier, Lambda, 
+    Quote, DoubleQuote, OpenBracket, CloseBracket, OpenParen, CloseParen,
+    OpenBrace, CloseBrace, Enum, Import, ClassWord, Public, Private, Protected,
+    Struct, True, False, If, ElseIf, Else, While, For, In, Equal,
+    BinaryOperator, NameSpace
+};
+
+struct SigmaToken {
+    SigmaTokenType type;
+    std::string symbol;
+};
 
 // The Lexer For The Actual Programming Language
 class SigmaLexer {
+public:
+    std::unordered_map<std::string, SigmaTokenType> known_tokens = {
+        {"var", Var}, {"lambda", Lambda}, {"\"", DoubleQuote}, {"\'", Quote},
+        {"[", OpenBracket}, {"]", CloseBracket}, {"(", OpenParen}, {")", CloseParen},
+        {"{", OpenBrace}, {"}", CloseBrace}, {"enum", Enum}, {"import", Import},
+        {"class", ClassWord}, {"public", Public}, {"private", Private}, {"protected", Protected},
+        {"struct", Struct}, {"true", True}, {"false", False}, {"if", If}, {"elseif", ElseIf},
+        {"else", Else}, {"while", While}, {"for", For}, {"in", In},
+         {"namespace", NameSpace}, {"+", BinaryOperator}, {"-", BinaryOperator},
+         {"*", BinaryOperator}, {"/", BinaryOperator}, {"%", BinaryOperator},
+         {"=", BinaryOperator}, {">", BinaryOperator}, {"<", BinaryOperator},
+         {">=", BinaryOperator}, {"<=", BinaryOperator}, {"&", BinaryOperator},
+         {">>", BinaryOperator}, {"<<", BinaryOperator}, {"|", BinaryOperator},
+         {"^", BinaryOperator}, {"!=", BinaryOperator}, {"&&", BinaryOperator},
+         {"||", BinaryOperator}
+    };
+    std::unordered_set<char> skip_chars = {
+        ' ', '\t', '\n', '\r'
+    };
+    std::vector<SigmaToken> tokenize(std::string code);
 
+    std::string *actual_code = nullptr;
+    size_t current_pos = 0;
+    char current_char = '0';
+
+    void advance(){
+        if(current_char != '\0' && current_pos < actual_code->size() - 1){
+            current_char = (*actual_code)[++current_pos];
+        }
+    }
 };
